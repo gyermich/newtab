60e620508d78d27dba7be088fee07a4673fcf1d1
Galina <yermichevagalina@gmail.com>
2014-03-19 17:17:14 -0700

    the apps can be dragged

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
  $(function(){
    var widgets = [];
-   var appList = [];
-   var orderedArr = [];
-
 
    function biggestIcon(icons) {
     var biggest = 0;
@@ -16,108 +13,41 @@
     return biggest;
    }
 
-
    function apps(extensionInfos){
 
-    // check if ordered array exists, push the rest of them into an array
 
      for ( var xi in extensionInfos ){
-      i = biggestIcon(extensionInfos[xi].icons);
-
        if (extensionInfos[xi].isApp) {
-         appList.push('<div class="drag"><li class="new" id="'+
-                              extensionInfos[xi].id +
-                              '" "><img id="'+ extensionInfos[xi].id +
-                              '" src=' +
-                              extensionInfos[xi].icons[i].url +
-                              '></br>' +
-                              extensionInfos[xi].shortName +
-                              '</li></div>', 2,2);
-         console.log(applist);
+         i = biggestIcon(extensionInfos[xi].icons);
+
+         gridster.add_widget('<div class="title"><li class="new" id="'+
+                            extensionInfos[xi].id +
+                            '" "><img id="'+ extensionInfos[xi].id +
+                            '" src=' +
+                            extensionInfos[xi].icons[i].url +
+                            '></br>' +
+                            extensionInfos[xi].shortName +
+                            '</li></div>', 2,2);
         }
      }
-     order();
-     for ( var x = 0;  x < orderedArr.length; x++){
-      gridster.add_widget(orderedArr[x]);
-       }
    }
 
 
     chrome.management.getAll(apps);
 
+    var dragged = false;
 
-    function order() {
-      var savedOrder = localStorage.getItem('orderedArr');
-          alert('im here');
-
-    if (savedOrder)
-      orderedArr = savedOrder.split(',');
-
-    if (orderedArr.length > 0) {
-      for (var i = 0; i < orderedArr.length; i++) {
-        for (var x = 0; x < appList.Length; x++) {
-          if (orderedArr[i] !== appList[x]) {
-            orderedArr.push(appList[x]);
-            console.log(orderedArr);
-            }
-          }
-
+      $(this).on('mouseup', function(ev){
+        if (dragged) {
+          dragged = false;
         }
-      }
-      else {
-        orderedArr = appList;
-        localStorage.setItem('orderedArr', orderedArr);
-      }
-      return orderedArr;
-    }
-
-
-    // $('.title').mousedown(function(event){
-    //   event.stopPropagation();
-    // });
-
-   //  var dragging = 0;
-
-   //  $('.drag').mousedown(function() {
-   //      $(document).mousemove(function(){
-   //         dragging = 1;
-   //      });
-   //  });
-
-   //  $(document).mouseup(function(){
-   //      dragging = 0;
-   //      $(document).unbind('mousemove');
-   //  });
-
-   // $(this).on('click', function(ev){
-   //      if (dragging === 0){
-   //      launchApp(ev.target.id);
-   //      }
-   //      else {
-   //        return false;
-   //      }
-   //  });
-
-
-      $(this).on('click', function(ev){
-        launchApp(ev.target.id);
+        else {
+          launchApp(ev.target.id);
+        }
       });
-
- // $('.drag').draggable({
- //     start: function(event, ui) {
- //         $(this).addClass('noclick');
- //     }
- // });
-
- // $('.drag').click(function(event) {
- //     if ($(this).hasClass('noclick')) {
- //         $(this).removeClass('noclick');
- //     }
- //     else {
- //         // actual click event code
- //           launchApp(event.target.id);
- //     }
- // });
+      $(this).on('mousemove', function(ev){
+        dragged = true;
+        });
 
 
     function launchApp(appID) {
@@ -131,4 +61,3 @@
       }).data('gridster');
 
     });
-
