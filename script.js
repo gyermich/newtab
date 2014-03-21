883b0748f4f04158c69a70bf1f9943f2f811d190
Galina <yermichevagalina@gmail.com>
2014-03-20 22:48:13 -0700

    app order is saved locally

--- script.js
+++ script.js
@@ -1,10 +1,36 @@
  $(function(){
    var widgets = [],
        dragged = false;
-       // orderedArr = localStorage.getItem('orderedArr');
-       // orderedArr = JSON.parse(orderedArr);
 
-       // console.log(orderedArr);
+
+       gridster = $(".gridster > ul").gridster({
+           widget_margins: [5, 5],
+           widget_base_dimensions: [100, 55],
+               serialize_params: function($w, wgd) {
+               return {
+                   x: wgd.col,
+                   y: wgd.row,
+                   width: wgd.size_x,
+                   height: wgd.size_y,
+                   id: $($w).attr('id'),
+                   class: $($w).attr('class'),
+                   html: $($w)[0].innerHTML,
+               };
+           },
+
+           draggable: {
+                       start: function(event, ui) {
+                                  dragged = true;
+                          },
+                       stop: function(event, ui){
+                         $oWidgets = this.serialize();
+                         localStorage.removeItem("orderedArr");
+                         localStorage.setItem("orderedArr", JSON.stringify($oWidgets));
+                         console.log($oWidgets);
+                       }
+                   }
+
+       }).data('gridster');
 
 
        // *   *   *   *   *   *   *   *   *   *   *   *   *
@@ -16,12 +42,25 @@
       //  OKOKoKOKOK
 
       //  soooooooo check if the order is stored in localStorage
-      //  if it is
-      //   first add_widget from saved order
+      if (localStorage.getItem('orderedArr') !== null) {
+        //  if it is
+        //   first add_widget from saved order
       // something like this
       // ------|||---|||---|||---|||---
-      // var json = JSON.parse(localStorage.getItem('orderedArr'));
-      // for (i = 0; i < json.length; i++) {
+        var json = JSON.parse(localStorage.getItem('orderedArr'));
+          for (i = 0; i < json.length; i++) {
+            console.log(json[i]['id']);
+            console.log(json[i]['html']);
+
+            gridster.add_widget('<li class="new" id="'+
+                                        json[i]['id']+
+                               '" ">' + json[i]['html'] +
+                               '</li>', json[i]['width'],
+                                        json[i]['height'],
+                                        json[i]['x'],
+                                        json[i]['y']
+                               );
+          }
       //         grid.add_widget(
       //             '<div id="' + json[i]['id'] + '"></div>',
       //         json[i]['size_x'],
@@ -32,10 +71,53 @@
       // -------___-------___-------___-------
 
       // THEN!!!! compare widget id's and if id not there
-      // add_widget to gridster
+        chrome.management.getAll( function(extensionInfos){
+          for ( var xi in extensionInfos ){
+            for ( var x in json ){
+              if (extensionInfos[xi].isApp) {
+                if (extensionInfos[xi].id  != json[x]['id']) {
+
+                  i = biggestIcon(extensionInfos[xi].icons);
+
+                  // add_widget to gridster
+                  // gridster.add_widget('<li class="new" id="'+
+                  //                    extensionInfos[xi].id +
+                  //                    '" "><img id="'+ extensionInfos[xi].id +
+                  //                    '" src=' +
+                  //                    extensionInfos[xi].icons[i].url +
+                  //                    '></br>' +
+                  //                    extensionInfos[xi].shortName +
+                  //                    '</li>', 2,2);
+                  }
+                }
+             }
+          }
+
+        });
+
+        // if order is not stored in localstorage just render the apps
+    } else {
+      chrome.management.getAll(function(extensionInfos){
 
 
-      // if order is not stored in localstorage just render the apps
+     for ( var xi in extensionInfos ){
+       if (extensionInfos[xi].isApp) {
+         i = biggestIcon(extensionInfos[xi].icons);
+
+
+         gridster.add_widget('<li class="new" id="'+
+                            extensionInfos[xi].id +
+                            '" "><img id="'+ extensionInfos[xi].id +
+                            '" src=' +
+                            extensionInfos[xi].icons[i].url +
+                            '></br>' +
+                            extensionInfos[xi].shortName +
+                            '</li>', 2,2);
+        }
+     }
+   });
+    }
+
       //   TADA!
 
 
@@ -55,28 +137,28 @@
     return biggest;
    }
 
-   function apps(extensionInfos){
+   // function apps(extensionInfos){
 
 
-     for ( var xi in extensionInfos ){
-       if (extensionInfos[xi].isApp) {
-         i = biggestIcon(extensionInfos[xi].icons);
+   //   for ( var xi in extensionInfos ){
+   //     if (extensionInfos[xi].isApp) {
+   //       i = biggestIcon(extensionInfos[xi].icons);
 
 
-         gridster.add_widget('<li class="new" id="'+
-                            extensionInfos[xi].id +
-                            '" "><img id="'+ extensionInfos[xi].id +
-                            '" src=' +
-                            extensionInfos[xi].icons[i].url +
-                            '></br>' +
-                            extensionInfos[xi].shortName +
-                            '</li>', 2,2);
-        }
-     }
-   }
+   //       gridster.add_widget('<li class="new" id="'+
+   //                          extensionInfos[xi].id +
+   //                          '" "><img id="'+ extensionInfos[xi].id +
+   //                          '" src=' +
+   //                          extensionInfos[xi].icons[i].url +
+   //                          '></br>' +
+   //                          extensionInfos[xi].shortName +
+   //                          '</li>', 2,2);
+   //      }
+   //   }
+   // }
 
 
-    chrome.management.getAll(apps);
+   //  chrome.management.getAll(apps);
     // order();
 
       $(this).on('mouseup', function(ev){
@@ -97,38 +179,7 @@
 
 
 
-      gridster = $(".gridster > ul").gridster({
-          widget_margins: [5, 5],
-          widget_base_dimensions: [100, 55],
-              serialize_params: function($w, wgd) {
-                console.log($w, wgd);
-              return {
-                  x: wgd.col,
-                  y: wgd.row,
-                  width: wgd.size_x,
-                  height: wgd.size_y,
-                  id: $($w).attr('id'),
-                  class: $($w).attr('class')
-                  // id: "aohghmighlieiainnegkcijnfilokake"
-                  // innerHTML: "<img id="aohghmighlieiainnegkcijnfilokake" src="chrome://extension-icon/aohghmighlieiainnegkcijnfilokake/128/0"><br>Google Docs"
-                  // innerText: "↵Google Docs"
-                  // textContent: "Google Docs"
-              };
-          },
 
-          draggable: {
-                      start: function(event, ui) {
-                                 dragged = true;
-                         },
-                      stop: function(event, ui){
-                        $oWidgets = this.serialize_changed();
-                        // saveWidgets($oWidgets);
-                        localStorage.setItem("orderedArr", JSON.stringify($oWidgets));
-                        console.log($oWidgets);
-                      }
-                  }
-
-      }).data('gridster');
 
 
       // function set_order(){
