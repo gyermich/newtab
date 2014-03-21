6584ee8ebd2e32c8a68091ecad0d107bd46bbf19
Galina <yermichevagalina@gmail.com>
2014-03-20 22:54:40 -0700

    refactor

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
    var widgets = [],
        dragged = false;
 
-
        gridster = $(".gridster > ul").gridster({
            widget_margins: [5, 5],
            widget_base_dimensions: [100, 55],
@@ -37,20 +36,11 @@
        //  * * * * * * * * * * * * * * * * * * * * * * * * *
        //   *   *   *   *   *   *   *   *   *   *   *   *   *
 
-
-
-      //  OKOKoKOKOK
-
-      //  soooooooo check if the order is stored in localStorage
+      //  check if the order is stored in localStorage
       if (localStorage.getItem('orderedArr') !== null) {
-        //  if it is
         //   first add_widget from saved order
-      // something like this
-      // ------|||---|||---|||---|||---
         var json = JSON.parse(localStorage.getItem('orderedArr'));
           for (i = 0; i < json.length; i++) {
-            console.log(json[i]['id']);
-            console.log(json[i]['html']);
 
             gridster.add_widget('<li class="new" id="'+
                                         json[i]['id']+
@@ -61,16 +51,8 @@
                                         json[i]['y']
                                );
           }
-      //         grid.add_widget(
-      //             '<div id="' + json[i]['id'] + '"></div>',
-      //         json[i]['size_x'],
-      //         json[i]['size_y'],
-      //         json[i]['col'],
-      //         json[i]['row']);
-      //     }
-      // -------___-------___-------___-------
 
-      // THEN!!!! compare widget id's and if id not there
+      // compare widget id's and if id not there
         chrome.management.getAll( function(extensionInfos){
           for ( var xi in extensionInfos ){
             for ( var x in json ){
@@ -116,7 +98,7 @@
         }
      }
    });
-    }
+  }
 
       //   TADA!
 
@@ -137,82 +119,17 @@
     return biggest;
    }
 
-   // function apps(extensionInfos){
-
-
-   //   for ( var xi in extensionInfos ){
-   //     if (extensionInfos[xi].isApp) {
-   //       i = biggestIcon(extensionInfos[xi].icons);
-
-
-   //       gridster.add_widget('<li class="new" id="'+
-   //                          extensionInfos[xi].id +
-   //                          '" "><img id="'+ extensionInfos[xi].id +
-   //                          '" src=' +
-   //                          extensionInfos[xi].icons[i].url +
-   //                          '></br>' +
-   //                          extensionInfos[xi].shortName +
-   //                          '</li>', 2,2);
-   //      }
-   //   }
-   // }
-
-
-   //  chrome.management.getAll(apps);
-    // order();
-
-      $(this).on('mouseup', function(ev){
-        if (dragged) {
-          // set_order();
-          dragged = false;
-        }
-        else {
-          launchApp(ev.target.id);
-        }
-      });
+    $(this).on('mouseup', function(ev){
+      if (dragged) {
+        dragged = false;
+      }
+      else {
+        launchApp(ev.target.id);
+      }
+    });
 
     function launchApp(appID) {
       chrome.management.launchApp(appID);
       window.close();
     }
-
-
-
-
-
-
-
-      // function set_order(){
-      //   $( "li" ).each(function( index ) {
-      //     // console.log( index + ": " + $( this ).text() );
-      //   });
-      // }
-
-
-      // function order() {
-      //   var orderedArr = [],
-      //       savedOrder = localStorage.getItem('orderedArr');
-
-      // if (savedOrder)
-      //   orderedArr = JSON.parse(savedOrder);
-
-      // if (orderedArr.length > 0) {
-      //   for (var i = 0; i < orderedArr.length; i++) {
-      //     for (var x = 0; x < gridster.Length; x++) {
-      //       if (orderedArr[i] !== gridster[x]) {
-      //         orderedArr.push(gridster[x]);
-      //         console.log(orderedArr);
-      //         }
-      //       }
-
-      //     }
-      //   }
-      //   else {
-      //     orderedArr = gridster;
-      //     localStorage.setItem('orderedArr', orderedArr);
-      //   }
-      //   return orderedArr;
-      // }
-
-
-    });
+  });
